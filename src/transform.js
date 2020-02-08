558610fe3c956d96f62cba98cb29604ad198a343
nikolasalim <nikolasalim@gmail.com>
2020-02-08 20:24:14 +0100

    clean up code

--- src/transform.js
+++ src/transform.js
@@ -42,32 +42,4 @@ const groupAdultsByAgeRange = array => {
   );
 };
 
-const testArr = [
-  { name: "pete", age: 10 },
-  { name: "dove", age: 17 },
-  { name: "dave", age: 18 },
-  { name: "hall", age: 19 },
-  { name: "donn", age: 20 },
-  { name: "trey", age: 21 },
-  { name: "hann", age: 29 },
-  { name: "chew", age: 30 },
-  { name: "cloe", age: 31 },
-  { name: "dart", age: 39 },
-  { name: "this", age: 40 },
-  { name: "dame", age: 41 },
-  { name: "henk", age: 49 },
-  { name: "anna", age: 50 },
-  { name: "olga", age: 51 },
-  { name: "bert", age: 52 },
-  { name: "oldd", age: 120 }
-];
-
-const testArr2 = [];
-
-const testArr3 = [
-  { name: "Anna", age: 31 },
-  { name: "John", age: 32 },
-  { name: "Hank", age: 60 }
-];
-
 module.exports = { groupAdultsByAgeRange };
